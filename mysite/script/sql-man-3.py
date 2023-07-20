51f5fb25f2de33c6f1d63f34d733fa711f7c4fbd
yangpan3 <yangpan3@jd.com>
2023-07-20 21:00:27 +0800

    docs: 根据提供的基础数据，生成 sql 脚本。

--- mysite/script/sql-man-3.py
+++ mysite/script/sql-man-3.py
@@ -1,12 +1,25 @@
-# -*- coding:utf-8 -*-
 
-pins = ""
-with open("C:\\Users\\yangpan3\\Desktop\\append.txt") as f:
-    for line in f:
-        if len(line.strip()) == 0:
-            continue
+"""
+根据提供的基础数据，生成 sql 脚本。 使用 gpt 生成。
+@since 2023-07-20 20:58:06
+"""
 
-        pins += "'" + line.strip() + "',"
+data = [
+    ['tommy', '太原'],
+    ['alice', '晋阳'],
+    # ... rest of the data
+]
 
-print(pins[0: len(pins) - 1])
+mapping = {
+    '太原': 597,
+    '晋阳': 598
+}
 
+if __name__ == '__main__':
+    for row in data:
+        erp = row[0]
+        department = row[1]
+        departmentId = mapping.get(department)
+
+        sql_statement = f"UPDATE table SET region_id = 423, battle_id = {departmentId} WHERE erp = '{erp}';"
+        print(sql_statement)
