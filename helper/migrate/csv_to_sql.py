7e6f9865c615bdd1c7da1a7c81db54b1d8bef437
yangpan23 <yangpan3@jd.com>
2023-10-26 16:37:19 +0800

    feat: 支持生成的 sql 写入文件，针对数据量大的情况 ✔

--- helper/migrate/csv_to_sql.py
+++ helper/migrate/csv_to_sql.py
@@ -14,10 +14,14 @@ def csv_to_insert_sql(csv_file):
         reader = csv.reader(file)
         headers = next(reader)  # header row
 
-        for row in reader:
-            values = ['"' + value + '"' if value else '""' for value in row]
-            sql = f"INSERT INTO {table_name} ({', '.join(headers)}) VALUES ({', '.join(values)});"
-            print(sql)
+        # 使用'a'模式打开文件，如果文件不存在，会自动创建
+        with open('output.txt', 'a') as f:
+            for row in reader:
+                values = ['"' + value + '"' if value else '""' for value in row]
+                sql = f"INSERT INTO {table_name} ({', '.join(headers)}) VALUES ({', '.join(values)});"
+                print(sql)
+                # 写入 sql 字符串并添加换行符
+                f.write(sql + '\n')
 
 
 if __name__ == '__main__':
