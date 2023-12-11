d71157e1367d74d0ae9f996b5c2e84bf5e94490f
yangpan23 <yangpan3@jd.com>
2023-12-11 17:48:28 +0800

    refactor: 调整目录, ai 单独工程

--- ai/samples.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-if __name__ == '__main__':
-    s = pd.Series(['a', 'a', 'b', 'c'])
-    s.describe()
-
-    data = pd.read_csv('https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv')
-    data.describe()
