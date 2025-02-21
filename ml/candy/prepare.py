1018551543047d2a91836239decda2db25988d1e
MrRobot5 <1050335971@qq.com>
2025-02-21 13:22:03 +0800

    feat: 数据预处理向量运算

--- ml/candy/prepare.py
+++ ml/candy/prepare.py
@@ -1,12 +1,13 @@
 
 """
-数据预处理（增加 target 列）
-处理规则： 使用未来3天的close - 当前close, 打标数据盈亏
+数据预处理（增加 target 列 "return"）
+处理规则： 使用未来 5天的close - 当前close, 打标数据盈亏
+    5天的数据对比，PnL 波动更加明显, 反之也更加灵敏
+
 源数据： input.csv (basic/http/loop_fetch.py)
 
 @since 2025年1月2日 17:35:39
 """
-
 
 import numpy as np
 import pandas as pd
@@ -15,8 +16,13 @@ import pandas as pd
 input_filename = 'input.csv'
 output_filename = 'train.csv'
 
+look_back = 3
+
 # 使用pandas读取CSV文件
 df = pd.read_csv(input_filename, parse_dates=['timestamp'])
+column_names_list = df.columns.tolist()
+print(column_names_list)
+
 # 将timestamp列转换为datetime类型
 # df['timestamp'] = pd.to_datetime(df['timestamp'])
 
@@ -27,17 +33,16 @@ df.sort_values(by='timestamp', ascending=True, inplace=True)
 df = df[df['timestamp'] > '2022-01-01']
 print(df.head(10))
 
-look_back = 3
-# 计算i+n行减去i行的“close”
-# 使用np.roll函数将“close”列向下移动n行，然后与原始列相减
-df['target'] = np.roll(df['close'],  - look_back) - df['close']
+# 使用shift方法直接计算差值，避免循环
+# for i in range(0, df.shape[0] - look_back):  # disgard the last "look_back" days
+#     result.append(df.iloc[i + look_back]['close'] - df.iloc[i]['close'])
+df['return'] = df['close'].shift(-look_back) - df['close']
 
-# 删除target列为NaN的行
-# 由于np.roll会导致前5行的target列为NaN，我们可以将这些值替换为一个特定的值或删除这些行
-# df = df.dropna(subset=['target'])
+# 删除最后look_back行的NaN值（因为shift操作会引入NaN）
+df = df.dropna(subset=['return'])
 
 # 输出到新的CSV文件
-# 删除roll 移动n行, 打标不对，Mean Squared Error 从1.6 提升到 0.8 @since 2025年1月9日 13:28:38
-df[:-look_back].to_csv(output_filename, index=False)
-
+# 删除roll 移动n行, 最后 n 行 return不对，Mean Squared Error 从1.6 提升到 0.8 @since 2025年1月9日 13:28:38
+# df[:-look_back].to_csv(output_filename, index=False)
+df.to_csv(output_filename, index=False)
 print(f"处理完成，结果已保存到 {output_filename}")
